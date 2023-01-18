24f20c6ed8e5a383d69b3fc084a0c0cff9726f7f
Ulissesortega <51983586+Ulissesortega@users.noreply.github.com>
2023-01-18 13:29:48 -0800

    Working JavaScrip Functions

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,24 @@
+let rock = document.getElementById("rock");
+let paper = document.getElementById("paper");
+let scissors = document.getElementById("scissors");
+let lizard = document.getElementById("lizard");
+let spock = document.getElementById("spock");
+
+
 let dataReturn = document.getElementById("dataReturn");
+let name = "Ulisea"
+let res;
 
 function GetData() {
   fetch("https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption")
   .then(response => response.text())
-  .then(data => dataReturn.textContent = data)
+  .then(data => {res = data;
+    console.log(res);
+  })
 }
 
+
+
 GetData();
+
+console.log(res);
