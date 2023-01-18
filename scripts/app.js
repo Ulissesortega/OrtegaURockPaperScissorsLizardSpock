b7694a7b216d527a671ac6026eaa010b15fa2ec2
Ulissesortega <51983586+Ulissesortega@users.noreply.github.com>
2023-01-18 14:58:10 -0800

    created Event Listeners

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,42 @@ let scissors = document.getElementById("scissors");
 let lizard = document.getElementById("lizard");
 let spock = document.getElementById("spock");
 
+let userInput;
+
+let res;
+
+
+
+rock.addEventListener("click", function(){
+   userInput = "rock"
+   console.log(userInput)
+   console.log(res)
+})
+
+paper.addEventListener("click", function(){
+  userInput = "paper"
+  console.log(userInput)
+})
+
+scissors.addEventListener("click", function(){
+  userInput = "scissors"
+  console.log(userInput)
+})
+
+lizard.addEventListener("click", function(){
+  userInput = "lizard"
+  console.log(userInput)
+})
+
+spock.addEventListener("click", function(){
+  userInput = "spock"
+  console.log(userInput)
+})
+
 
 let dataReturn = document.getElementById("dataReturn");
-let name = "Ulisea"
-let res;
+
+
 
 function GetData() {
   fetch("https://scottsrpsls.azurewebsites.net/api/RockPaperScissors/GetRandomOption")
@@ -17,8 +49,4 @@ function GetData() {
   })
 }
 
-
-
 GetData();
-
-console.log(res);
