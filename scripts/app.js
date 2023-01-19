13a1b5c99a73ecd70f81c04346986dbbb3b40943
Ulissesortega <51983586+Ulissesortega@users.noreply.github.com>
2023-01-18 21:40:12 -0800

    Updated Code Wording

--- scripts/app.js
+++ scripts/app.js
@@ -10,9 +10,6 @@ let userInput;
 let res;
 
 
-
-
-
 rock.addEventListener("click", function () {
   userInput = "rock"
   console.log(userInput);
@@ -93,14 +90,14 @@ function OutCome() {
     injectHere.textContent = "Paper Get Cut by Scissors, You Lose!"
   }
   else if (userInput == "paper" && res == "Lizard") {
-    injectHere.textContent = "Paper gets eaten by Lizard, You Lose!"
+    injectHere.textContent = "Paper Gets Eaten by Lizard, You Lose!"
   }
   else if (userInput == "paper" && res == "Spock") {
     injectHere.textContent = "Paper Disproves Spock, You Win!"
   }
 
   //Scissors
-  if (userInput == "scissors" && res == "scissors") {
+  if (userInput == "scissors" && res == "Scissors") {
     injectHere.textContent = "Scissors vs. scissors, This is a Tie!"
   }
   else if (userInput == "scissors" && res == "Rock") {
@@ -126,7 +123,7 @@ function OutCome() {
   else if (userInput == "lizard" && res == "Paper") {
     injectHere.textContent = "Lizard Eats Paper, You Win!"
   }
-  else if (userInput == "lizard" && res == "Scissor") {
+  else if (userInput == "lizard" && res == "Scissors") {
     injectHere.textContent = "Lizard Decapitated by Scissors, You Lose!"
   }
   else if (userInput == "lizard" && res == "Spock") {
@@ -134,7 +131,7 @@ function OutCome() {
   }
 
   //Spock
-  if (userInput == "spock" && res == "Lizard") {
+  if (userInput == "spock" && res == "Spock") {
     injectHere.textContent = "Spock vs. Spock, This is a Tie!"
   }
   else if (userInput == "spock" && res == "Rock") {
@@ -143,7 +140,7 @@ function OutCome() {
   else if (userInput == "spock" && res == "Paper") {
     injectHere.textContent = "Spock Disproved by Paper, You Lost!"
   }
-  else if (userInput == "spock" && res == "Scissor") {
+  else if (userInput == "spock" && res == "Scissors") {
     injectHere.textContent = "Spock Crushes Scissors, You Lose!"
   }
   else if (userInput == "spock" && res == "Lizard") {
